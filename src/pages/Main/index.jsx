ab0b600d978fb6ddad636626d6188b6518d8f3fa
kkwang <kkwang@wemakeprice.com>
2021-04-24 14:36:54 +0900

    feat(kkwang-#7) : semantic-ui 라이브러리 설치 및 환경 세팅

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 function Main() {
   return (
     <>
       <h1>Main page</h1>
+      <Button primary>Primary</Button>
+      <Button color="black">Black</Button>
     </>
   );
 }
