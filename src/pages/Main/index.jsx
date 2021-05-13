17441f8bd3b9ca65aeb1db0194572140de2c795b
kkwang <kkwang@wemakeprice.com>
2021-05-14 02:34:33 +0900

    feat(kkwang-#18) : api-mocker 추가설정

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -5,14 +5,11 @@ import AsideList from '@components/main/AsideList';
 import TodoList from '@components/main/TodoList';
 
 import { axiosLoader } from '@utils/axiosLoader';
-// mock data
-import todoData from '@mock/data/todoData';
 
 function Main() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [test, setTest] = useState([]);
   
   useEffect(() => {
     getListData();
@@ -20,42 +17,23 @@ function Main() {
 
   // 전체 리스트 조회
   const getListData = () => {
-    // axios test url
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    // const url = 'http://localhost:8081/api/content/v1/todos';
+    const url = '/api/content/v1/todos';
     
     setLoading(true);
     axiosLoader(url, {}, (res) => {
-      console.log('res', res);
-      setTest(res.data);
+      setData(res.data);
       setLoading(false);
-
     }, (err) => {
       setError(err);
     });
-
-    // mock data
-    setData(todoData);    
   };
 
-  
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다.</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;  
   
   console.log('data', data);
-  console.log('test', test);
-
   return (
     <>
-      <div>
-        <span>axios test data</span>
-        {
-          test.map(item => (
-              <li key={item.id}>{item.username}</li>
-            )
-          )
-        }
-      </div>
       <AsideList data={data} />
       <TodoList data={data} />
     </>
--- webpack/development.js
+++ webpack/development.js
@@ -1,3 +1,4 @@
+const apiMocker = require('connect-api-mocker')
 const { merge } = require('webpack-merge');
 const base = require('./base');
 
@@ -9,5 +10,23 @@ module.exports = merge(base, {
     port: 4000,
     historyApiFallback: true,
     hot: true,
+    overlay: true,
+    stats: 'errors-only',
+    before: (app) => {
+      // todos 전체조회(GET)
+      app.use(apiMocker('/api/content/v1/todos', './src/mock/todos'));
+      // dod 로그인(POST)
+      app.use(apiMocker('/api/v1/member/login', './src/mock/login'));
+      // dod 회원가입(POST)
+      app.use(apiMocker('/api/v1/member/join', './src/mock/join'));
+      // dod ID중복체크(GET)
+      app.use(apiMocker('/api/v1/member/check/duplicate', './src/mock/join'));
+    },
+    proxy: {
+      '/api': {
+        target: 'http:localhost:8081',
+        changeOrigin: true
+      }
+    }
   },
 });
