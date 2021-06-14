88d2d8bf34091e6582b7881e18e680df9b4eba49
kkwang <kkwang@wemakeprice.com>
2021-06-14 20:03:15 +0900

    feat(kkwang-#41) : modal component draft

--- src/components/main/SideList/index.jsx
+++ src/components/main/SideList/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import Button from '@components/common/Button';
 
-function SideList({ children }) {
+function SideList({ onAddItem, children }) {
   return (
-    <div className='todo_list'>
+    <>
       <div className='todo_title'>
         <h3>할일 목록</h3>
+        <Button className='add_ico' onClick={onAddItem}/>
       </div>
-      <ul>
-        {children}
-      </ul>
-    </div>
+      <div className='todo_list'>
+        <ul>
+          {children}
+        </ul>
+      </div>
+    </>
   );
 }
 
